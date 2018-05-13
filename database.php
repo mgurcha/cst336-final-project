aed3e5042c0990529f2b2ea1cc676a26cdc7798f
Manjit Gurcha <mgurcha@csumb.edu>
2018-05-13 01:35:39 +0000

    Adding all files

--- database.php
+++ database.php
@@ -9,6 +9,7 @@ function getDatabaseConnection()
 
 // Create connection
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conn;
     
   }
